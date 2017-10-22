ea3b82519ed0b4d18fac3ec56ae9adf072bd663f
Dylan Simmer <goldenoblivion@gmail.com>
2017-10-22 18:44:56 +1100

    update claim to reserve

--- synced/role.claimer.js
+++ synced/role.claimer.js
@@ -7,7 +7,12 @@ var roleClaimer = {
 		}
         if (creep.memory.claimTarget) {
             const err = creep.claimController(creep.room.controller);
-            if(err == ERR_NOT_IN_RANGE || err == ERR_INVALID_TARGET) {
+            if (err == ERR_NOT_IN_RANGE || err == ERR_INVALID_TARGET) {
+                controllerpos = new RoomPosition(claimTarget.x, claimTarget.y, claimTarget.room)
+                creep.moveTo(controllerpos);
+            }
+            if (err = ERR_GCL_NOT_ENOUGH) {
+                creep.reserveController(creep.room.controller);
                 controllerpos = new RoomPosition(claimTarget.x, claimTarget.y, claimTarget.room)
                 creep.moveTo(controllerpos);
             }
