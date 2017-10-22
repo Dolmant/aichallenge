c0488bdf220aa461b2601030c3ceea940362e971
Dylan Simmer <goldenoblivion@gmail.com>
2017-10-22 17:12:02 +1100

    fix claim

--- synced/role.claimer.js
+++ synced/role.claimer.js
@@ -7,7 +7,7 @@ var roleClaimer = {
 		}
 
         if (creep.room.controller.my) {
-            creep.moveTo(26, 49);
+            creep.moveTo(creep.pos.findClosestByRange(creep.room.findExitTo(creep.memory.claimTarget)));
         } else {
             if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
