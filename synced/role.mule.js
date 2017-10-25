56c8d17187ac9c3950ce6958c2a5c8bfb82972f0
Dylan Simmer <goldenoblivion@gmail.com>
2017-10-26 00:30:11 +1100

    fix mule

--- synced/role.mule.js
+++ synced/role.mule.js
@@ -30,29 +30,46 @@ var roleMule = {
 };
 function getEnergy(creep) {
     if (!creep.memory.fetchTarget && !creep.memory.dropTarget) {
-        var target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
-        if (target) {
-            creep.memory.dropTarget = target.id;
-        } else {
-            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_CONTAINER) && (structure.store.energy > 100);
-                }
-            });
-            if (target) {
-                creep.memory.fetchTarget = target.id;
-            }
-        }
+        getTargets(creep);
     }
+    var target = 0;
     if (creep.memory.dropTarget) {
         target = Game.getObjectById(creep.memory.dropTarget);
-        if(target && creep.pickup(target) == ERR_NOT_IN_RANGE) {
+        var err = target && creep.pickup(target);
+        if (err == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
+        } else if (err == OK) {
+            creep.memory.dropTarget = 0
+        } else {
+            getTargets(creep);
         }
     } else if (creep.memory.fetchTarget) {
         target = Game.getObjectById(creep.memory.fetchTarget);
-        if(target && creep.withdraw(target) == ERR_NOT_IN_RANGE) {
+        var err = target && creep.pickup(target);
+        if (err == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
+        } else if (err == OK) {
+            creep.memory.fetchTarget = 0
+        } else {
+            getTargets(creep);
+        }
+    }
+}
+
+function getTargets(creep) {
+    var target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+    if (target) {
+        creep.memory.dropTarget = target.id;
+        creep.memory.fetchTarget = 0;
+    } else {
+        target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_CONTAINER) && (structure.store.energy > 100);
+            }
+        });
+        if (target) {
+            creep.memory.fetchTarget = target.id;
+            creep.memory.dropTarget = 0;
         }
     }
 }
