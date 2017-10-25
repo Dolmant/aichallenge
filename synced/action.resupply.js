7e7d2d4292f0e552cbf266e06bd1b9898cf65450
Dylan Simmer <goldenoblivion@gmail.com>
2017-10-25 23:44:38 +1100

    fix builder again

--- synced/action.resupply.js
+++ synced/action.resupply.js
@@ -8,18 +8,13 @@ var actResupply = {
         //it's been a while since I looked into this, jeeze this eats up a lot of CPU, this should check once then head towards it
         //TODO: make this not suck CPU, save the target etc.
         if (!creep.memory.resupplyTarget) {
-            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return ((structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_LINK) && (structure.energy < 0 || (structure.storeCapacity && structure.store.energy < 0)));
-                }
-            });
-            if (target) {
-                creep.memory.resupplyTarget = target.id;
-            }
+            getResupplyTarget(creep);
         }
         if(creep.memory.resupplyTarget) {
             var resupplyTarget = Game.getObjectById(creep.memory.resupplyTarget);
-            if(resupplyTarget && creep.withdraw(resupplyTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            if (!resupplyTarget) {
+                getResupplyTarget(creep);
+            } else if(creep.withdraw(resupplyTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(resupplyTarget, {visualizePathStyle: {stroke: '#ffffff'}});
             }
         } else {
@@ -27,5 +22,16 @@ var actResupply = {
         }
     }
 };
+
+function getResupplyTarget(creep) {
+    var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (structure) => {
+            return ((structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_LINK) && (structure.energy < 0 || (structure.storeCapacity && structure.store.energy < 0)));
+        }
+    });
+    if (target) {
+        creep.memory.resupplyTarget = target.id;
+    }
+}
 
 module.exports = actResupply;
