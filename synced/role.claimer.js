6707628ed13e1b7217381ba174f047a0484831db
Dylan Simmer <goldenoblivion@gmail.com>
2017-10-27 22:12:04 +1100

    fix claimer

--- synced/role.claimer.js
+++ synced/role.claimer.js
@@ -1,22 +1,26 @@
+var util = require('util');
 var roleClaimer = {
-
-    /** @param {Creep} creep **/
     run: function(creep) {
 		if (creep.fatigue != 0){
 			return;
         }
 
         if (Game.flags['Claim']) {
-            let err = creep.claimController(Game.flags['Claim'].room.controller);
-            if (err == ERR_INVALID_TARGET) {
-                err = creep.attackController(Game.flags['Claim'].room.controller);
-            }
-            if (err == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.flags['Claim'].pos);
-            }
-            if (err == ERR_GCL_NOT_ENOUGH) {
-                creep.reserveController(creep.room.controller);
-                creep.moveTo(Game.flags['Claim'].pos);
+            if (creep.room.name == Game.flags['Claim'].pos.roomName) {
+                let err = creep.claimController(creep.room.controller);
+                if (err == ERR_INVALID_TARGET) {
+                    err = creep.attackController(creep.room.controller);
+                }
+                if (err == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.flags['Claim'].pos);
+                }
+                if (err == ERR_GCL_NOT_ENOUGH) {
+                    creep.reserveController(creep.room.controller);
+                    creep.moveTo(Game.flags['Claim'].pos);
+                }
+            } else {
+                creep.memory.goToTarget = Game.flags['Claim'].pos.roomName;
+                util.goToTarget(creep);
             }
         }
 	}
