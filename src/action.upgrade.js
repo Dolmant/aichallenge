d17767104a55bd8a1af9adb41a34aa4e17627b6d
Dylan Simmer <goldenoblivion@gmail.com>
2017-10-28 16:58:15 +1100

    webpack fix

--- src/action.upgrade.js
+++ src/action.upgrade.js
@@ -1,13 +1,9 @@
 var actUpgrade = {
-    
-    /** @param {Creep} creep **/
-    
-    /** @param {Creep} creep **/
     run: function(creep) {
         if( creep.memory.MyController == undefined){
             creep.memory.MyController = creep.room.controller.id;
         }
-        myUpgrade = Game.getObjectById(creep.memory.MyController);
+        let myUpgrade = Game.getObjectById(creep.memory.MyController);
         if(creep.upgradeController(myUpgrade) == ERR_NOT_IN_RANGE) {
             creep.moveTo(myUpgrade, {visualizePathStyle: {stroke: '#ffffff'}});
         }
