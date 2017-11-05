5532c82d3bdc5551b84baa05c3b1cb2947a93eb5
Dylan Simmer <goldenoblivion@gmail.com>
2017-11-05 20:51:30 +1100

    room fixes

--- synced/main.js
+++ synced/main.js
@@ -71,14 +71,39 @@ module.exports =
 "use strict";
 const util = {
     goToTarget(creep) {
+        var err = 0;
         if (creep.pos.x == 0) {
-            creep.move(RIGHT);
+            err = creep.move(RIGHT);
+            if (err != OK) {
+                err = creep.move(TOP_RIGHT);
+            }
+            if (err != OK) {
+                err = creep.move(BOTTOM_RIGHT);
+            }
         } else if (creep.pos.x == 49) {
-            creep.move(LEFT);
+            err = creep.move(LEFT);
+            if (err != OK) {
+                err = creep.move(TOP_LEFT);
+            }
+            if (err != OK) {
+                err = creep.move(BOTTOM_LEFT);
+            }
         } else if (creep.pos.y == 0) {
-            creep.move(BOTTOM);
+            err = creep.move(BOTTOM);
+            if (err != OK) {
+                err = creep.move(BOTTOM_LEFT);
+            }
+            if (err != OK) {
+                err = creep.move(BOTTOM_RIGHT);
+            }
         } else if (creep.pos.y == 49) {
-            creep.move(TOP);
+            err = creep.move(TOP);
+            if (err != OK) {
+                err = creep.move(TOP_RIGHT);
+            }
+            if (err != OK) {
+                err = creep.move(TOP_LEFT);
+            }
         } else if (creep.room.name == creep.memory.goToTarget) {
             delete creep.memory.goToTarget;
             delete creep.memory.myTask;
