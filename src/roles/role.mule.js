6a900b07685cbbc0104747a46c995f0308743b9f
Dylan Simmer <goldenoblivion@gmail.com>
2017-12-04 19:33:08 +1100

    mule .75 treatment

--- src/roles/role.mule.js
+++ src/roles/role.mule.js
@@ -5,10 +5,10 @@ const roleMule = {
         if (creep.fatigue != 0){
             return;
         }
-        if(_.sum(creep.carry) < creep.carryCapacity) {
+        if(_.sum(creep.carry) < creep.carryCapacity * 0.75) {
             creep.memory.myTask = 'fetch';
             creep.memory.depositTarget = 0;
-        } else if (_.sum(creep.carry) == creep.carryCapacity) {
+        } else if (_.sum(creep.carry) >= creep.carryCapacity * 0.75) {
             creep.memory.fetchTarget = 0;
             creep.memory.dropTarget = 0;
             creep.memory.myTask = 'deposit';
